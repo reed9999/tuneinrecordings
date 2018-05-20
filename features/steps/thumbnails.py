3e6dd84568deeb8035782d7d2b0b9dc7bbdba80c
reed9999 <reed9999@uw.edu>
2018-05-20 01:54:47 -0700

    Lots of skips. Reworking the thumbnails steps.

--- features/steps/thumbnails.py
+++ features/steps/thumbnails.py
@@ -7,76 +7,6 @@ from shutil import copyfile, copy2, copytree, rmtree
 
 use_step_matcher("re")
 
-
-@given("There are no lingering output files")
-def step_impl(context):
-    if os.path.isfile("./thumbnails.html"):
-        # os.remove("./thumbnails.html")
-        pass
-    dir = "tests/testbed/recordings"
-    if os.path.isfile(dir + "/thumbnails.html"):
-        # os.remove(dir + "/thumbnails.html")
-        pass
-
-@given("Individual recordings are present in (?P<testbed>[^ ]*)")
-def step_impl(context, testbed):
-    """
-    :type context: behave.runner.Context
-    """
-    testbed = "tests/testbed"
-    dst = os.path.join(testbed, "recordings")
-    store = os.path.join(testbed, "__store/recordings")
-    for fn in glob.glob(pathname=os.path.join(store, "15*")):
-        dst_file = os.path.join(dst, os.path.basename(fn))
-        try:
-            rmtree(dst_file)
-        except FileNotFoundError:
-            print("{} DNE".format(dst_file))
-        except:
-            raise
-        copytree(fn, dst_file)
-
-
-@given("Subdirs with recordings are present in (?P<testbed>[^ ]*)")
-def step_impl(context, testbed):
-    """
-    :type context: behave.runner.Context
-    """
-
-    testbed = "tests/testbed"
-    dst = os.path.join(testbed, "recordings/2018-03")
-    store = os.path.join(testbed, "__store/recordings/2018-03")
-    try:
-        rmtree(dst)
-    except FileNotFoundError:
-        print("{} DNE".format(dst))
-    except:
-        raise
-    copytree(store, dst)
-
-
-@step("I run the app")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-
-    Step (which should probably really be a conventional unit test instead) to run the app
-    without any parameter.
-    """
-    app = context.app
-    app.go()
-
-@when("I run the app passing in the name (?P<testbed>[^ ]*)")
-def step_impl(context, testbed):
-    """
-    :type context: behave.runner.Context
-
-    Step (which should probably really be a conventional unit test instead) to run the app
-    with a stated parameter.
-    """
-    #context.testbed = testbed
-    context.scenario.skip()
-
 def all_imgs_in(the_file):
     """
     :returns dict of the src and alt attributes for all img tags in the file given
@@ -114,8 +44,79 @@ def assert_all_img_and_alt_present(output_file, testbed, recursive=True):
         raise NotImplementedError
 
 
-@then("I get an HTML file (?P<output_file>.*) with img and alt for all thumbnails in (?P<testbed>[^ .]*)\.?")
-def step_impl(context, output_file, testbed):
+
+@given("[Tt]here are no lingering output files")
+def step_impl(context):
+    if os.path.isfile("./thumbnails.html"):
+        # os.remove("./thumbnails.html")
+        pass
+    dir = "tests/testbed/recordings"
+    if os.path.isfile(dir + "/thumbnails.html"):
+        # os.remove(dir + "/thumbnails.html")
+        pass
+
+@given("individual recordings are present in (?P<input_place>.*) place")
+def step_impl(context, input_place):
+    """
+    :type context: behave.runner.Context
+    """
+    context.scenario.skip()
+    testbed = "tests/testbed"
+    dst = os.path.join(testbed, "recordings")
+    store = os.path.join(testbed, "__store/recordings")
+    for fn in glob.glob(pathname=os.path.join(store, "15*")):
+        dst_file = os.path.join(dst, os.path.basename(fn))
+        try:
+            rmtree(dst_file)
+        except FileNotFoundError:
+            print("{} DNE".format(dst_file))
+        except:
+            raise
+        copytree(fn, dst_file)
+
+
+@given("XXX")
+def step_impl(context, testbed):
+    """
+    :type context: behave.runner.Context
+    """
+
+    testbed = "tests/testbed"
+    dst = os.path.join(testbed, "recordings/2018-03")
+    store = os.path.join(testbed, "__store/recordings/2018-03")
+    try:
+        rmtree(dst)
+    except FileNotFoundError:
+        print("{} DNE".format(dst))
+    except:
+        raise
+    copytree(store, dst)
+
+
+@when("I run the app")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+
+    Step (which should probably really be a conventional unit test instead) to run the app
+    without any parameter.
+    """
+    app = context.app
+    app.go()
+
+@when("I run the app passing in the name (?P<testbed>[^ ]*)")
+def step_impl(context, testbed):
+    """
+    :type context: behave.runner.Context
+
+    Step (which should probably really be a conventional unit test instead) to run the app
+    with a stated parameter.
+    """
+    #context.testbed = testbed
+    context.scenario.skip()
+
+@then("I get an HTML output file in (?P<output_place>) output place")
+def step_impl(context, output_place):
     """
     :type context: behave.runner.Context
     """
@@ -136,6 +137,13 @@ def all_img_attributes_in(f):
     img_alts = [i['alt'] for i in img_dicts]
     return img_srcs, img_alts
 
+
+@then("the output file displays images for img tags in the default place and subdirs\.?")
+def step_impl(context, output_file, testbed):
+    """
+    :type context: behave.runner.Context
+    """
+    assert os.path.isfile(output_file), "File {} does not exist".format(output_file)
 
 @then("I get an HTML file (?P<output_file>.*) with img and alt for all thumbnails in (?P<testbed>.*) and subdirs\.?")
 def step_impl(context, output_file, testbed):
--- features/thumbnails.feature
+++ features/thumbnails.feature
@@ -6,44 +6,53 @@
 # At least that is my reading of BDD.
 #####
 
-@fixture.app #Doesn't seem to be read
+@fixture.app
 Feature: Retrieve thumbnail images from an arbitrary path to assist in figuring
   out what I have.
 
   #These are more like unit testcases but for while I'm getting a feel for BDD I think
   # it's OK to start with this kind of stuff.
 
-  @fixture.app
-  Scenario: Retrieve thumbnails for hardcoded non-recursive path
-    Given There are no lingering output files
-    And Individual recordings are present in tests/testbed-working/recordings
-    When I run the app
-#    Then I get an HTML file tests/output/thumbnails.html allowing me to view all thumbnails in tests/testbed-working/recordings
-    Then I get an HTML file ./thumbnails.html with img and alt for all thumbnails in tests/testbed-working/recordings.
-
+#  Scenario: Retrieve thumbnails for hardcoded non-recursive path
+#Who cares? It wasn't any harder to implement the recursive one.
 
   Scenario: Retrieve thumbnails for hardcoded recursive path
-    Given There are no lingering output files
-    And Subdirs with recordings are present in tests/testbed-working/recordings
-    When I run the app
-    Then I get an HTML file ./thumbnails.html with img and alt for all thumbnails in tests/testbed-working/recordings and subdirs
+    If everything is in a default place, we get a nice tidy HTML file.
+    NOTE 1: The HTML file is not that tidy yet. Add some criteria for what I want.
+    NOTE 2: It's weird that the test dir should be the default. Really needs a better default.
+    Although in fairness it could be ".", the current dir, whatever.
 
-  @fixture.app
+    Given there are no lingering output files
+    And individual recordings are present in the default input place
+    When I run the app
+    Then I get an HTML output file in the default output place
+    And the output file displays images for img tags in the default place and subdirs
+    And the output file gives directory locations for images displayed
+    And the images have alt tags for all alt tags in the default place and subdirs.
+
   Scenario: Retrieve thumbnails for hardcoded recursive path storing in arbitrary output file
-    Given There are no lingering output files
-    And Subdirs with recordings are present in tests/testbed-working/recordings
-    When I run the app with output file ./recordings/output/thumbnails.html
-    #Really something like this should be default anyway.
-    Then I get an HTML file ./recordings/output/thumbnails.html with img and alt for all thumbnails in tests/testbed-working/recordings and subdirs
+    As above, but it should work even if the output file isn't the default place.
+
+    Given there are no lingering output files
+    And individual recordings are present in the default input place
+    When I run the app
+    Then I get an HTML output file in an arbitrary output place
+    And the output file displays images for img tags in the default place and subdirs
+    And the images have alt tags for all alt tags in the default place and subdirs.
 
   Scenario: Retrieve thumbnails for arbitrary recursive path
-    Given There are no lingering output files
-    And Subdirs with recordings are present in tests/testbed/another-path
-    When I run the app passing in the name tests/testbed/another-path
-    Then I get an HTML file ./thumbnails.html allowing me to view all thumbnails in tests/testbed/another-path and subdirs
-#    Then I get an HTML file tests/output/thumbnails.html with relative paths allowing me to view all thumbnails including subdirs.
+    As above, but it should also work even if the input files aren't in the default place.
+
+    Given there are no lingering output files
+    And individual recordings are present in an arbitrary input place
+    When I run the app
+    Then I get an HTML output file in an arbitrary output place
+    And the output file displays images for img tags in the arbitrary place and subdirs
+    And the images have alt tags for all alt tags in abitrary place and subdirs.
 
   Scenario: Use the GUI to assign names to images
+    The GUI doesn't yet exist so expecting it to do something is absurd.
+
     Given the GUI is running
     When I invoke the image naming dialogue
     And I use the GUI to assign names to images

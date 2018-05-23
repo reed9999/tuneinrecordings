e2c0045b999b651d879bf617b1cccf6ff81bf08e
reed9999 <reed9999@uw.edu>
2018-05-23 09:50:24 -0700

    Troubleshooting stuff for Travis

---
+++ features/steps/tshoot.py
@@ -0,0 +1,40 @@
+import os
+import glob
+import re
+from tests.results import TEST_RESULTS
+#from hamcrest import assert_that, equal_to
+from lxml import html
+from shutil import copyfile, copy2, copytree, rmtree
+
+use_step_matcher("re")
+
+THIS_FILE_DIR = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.join(THIS_FILE_DIR, '..', '..')
+PATHS = {
+    #Nested better than tuple approach, I can tell. Need to retrofit slightly.
+    'default': {'input': os.path.join(PROJECT_ROOT, 'tests', 'testbed-working', 'recordings'),
+        'output': os.path.join(PROJECT_ROOT, 'thumbnails.html')},
+    ('output', 'default'): os.path.join(PROJECT_ROOT, 'tests', 'another-arbitrary-dir', 'arbitrary-filename.txt'),
+    ('input', 'default'): os.path.join(PROJECT_ROOT, 'tests', 'testbed-working', 'recordings'),
+    ('output', 'default'): os.path.join(PROJECT_ROOT, 'thumbnails.html'),
+    ('input', 'arbitrary'): os.path.join(PROJECT_ROOT, 'tests', 'arbitrary-name'),
+    ('output', 'default'): os.path.join(PROJECT_ROOT, 'tests', 'another-arbitrary-dir', 'arbitrary-filename.txt'),
+    'store': os.path.join(PROJECT_ROOT, 'tests', 'testbed-store'),
+}
+def skip(context):
+    context.scenario.skip()
+
+@then(u'ls')
+def step_impl(context):
+    path = os.path.join(THIS_FILE_DIR, '..', '..', 'tests', 'testbed-working')
+    for (root, dn, fn) in os.walk(path):
+        print("root {} dn {} fn {}".format(root, dn, fn))
+
+
+@when(u'tshoot')
+def step_impl(context):
+    path = os.path.join(THIS_FILE_DIR, '..', '..', 'tests', 'testbed-working')
+    print("Eventually maybe run the app here or something")
+    for (root, dn, fn) in os.walk(path):
+        print("root {} dn {} fn {}".format(root, dn, fn))
+
---
+++ features/tshoot.feature
@@ -0,0 +1,16 @@
+#####
+# This is only to troubleshoot why things fail on travis-ci servers that do
+# not fail on my own test environment.
+#####
+
+Feature: Troubleshoot
+
+  Scenario: Tshoot 1
+    Given there are no lingering output files
+    And individual recordings are present in the default input place
+    Then ls
+
+    Given there are no lingering output files
+    And individual recordings are present in the default input place
+    When tshoot
+    Then ls
